ccf06e101c54dcfd08bd401905dad0d245cf7890
davinreyhans <[daavinreyhans@gmail.com]>
2023-04-08 17:26:52 +0700

    first commit

---
+++ src/test/java/com/example/tiketbioskop/TiketBioskopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiketbioskop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiketBioskopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
