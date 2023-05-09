32fdc6a8621ee361ad385562bd51b1bdd5745d24
davinreyhans <[daavinreyhans@gmail.com]>
2023-05-09 21:27:38 +0700

    update repo

--- src/main/java/com/example/tiketbioskop/repository/FilmsRepository.java
+++ src/main/java/com/example/tiketbioskop/repository/FilmsRepository.java
@@ -2,6 +2,7 @@ package com.example.tiketbioskop.repository;
 
 import com.example.tiketbioskop.model.Films;
 import com.example.tiketbioskop.model.Users;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
+@Transactional
 public interface FilmsRepository extends JpaRepository<Films, Integer> {
 //    List<Films> findAll();
     Films findFilmByFilmId(Integer filmId);
--- src/main/java/com/example/tiketbioskop/repository/UsersRepository.java
+++ src/main/java/com/example/tiketbioskop/repository/UsersRepository.java
@@ -1,12 +1,14 @@
 package com.example.tiketbioskop.repository;
 
 import com.example.tiketbioskop.model.Users;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public interface UsersRepository extends JpaRepository<Users, Integer> {
     List<Users> findAll();
     Users findByUserId(Integer userId);
