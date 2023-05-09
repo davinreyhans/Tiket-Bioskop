23d1b09fbabb6b94baa53cd5d57d7af3a5284577
davinreyhans <[daavinreyhans@gmail.com]>
2023-05-10 01:17:18 +0700

    add NotNull to column film_code, film_name, and is_showing

--- src/main/java/com/example/tiketbioskop/model/Films.java
+++ src/main/java/com/example/tiketbioskop/model/Films.java
@@ -1,6 +1,7 @@
 package com.example.tiketbioskop.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -14,12 +15,14 @@ public class Films {
     @Column(name = "film_id")
     private Integer filmId;
 
-    @Column(name = "film_code")
+    @NotNull
+    @Column(name = "film_code", length = 3)
     private String filmCode;
 
     @Column(name = "film_name")
     private String filmName;
 
+    @NotNull
     @Column(name = "is_showing")
     private Boolean isShowing;
 }
