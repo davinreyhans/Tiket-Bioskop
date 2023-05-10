27854f937914b81935508c3ea4d20e60e4c097df
davinreyhans <[daavinreyhans@gmail.com]>
2023-05-10 22:00:17 +0700

    change column definition

--- src/main/java/com/example/tiketbioskop/model/Schedules.java
+++ src/main/java/com/example/tiketbioskop/model/Schedules.java
@@ -12,22 +12,22 @@ import lombok.Setter;
 public class Schedules {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "schedules_id")
-    private Integer schedulesId;
+    @Column(name = "schedule_id", nullable = false)
+    private Integer scheduleId;
 
     @ManyToOne(targetEntity = Films.class)
     @JoinColumn(name = "film_id", nullable = false)
     private Films filmId;
 
-    @Column(name = "film_date")
+    @Column(name = "film_date", nullable = false)
     private String filmDate;
 
-    @Column(name = "film_start_time")
+    @Column(name = "film_start_time", nullable = false)
     private String filmStartTime;
 
-    @Column(name = "film_end_time")
+    @Column(name = "film_end_time", nullable = false)
     private String filmEndTime;
 
-    @Column(name = "ticket_price")
+    @Column(name = "ticket_price", nullable = false)
     private Integer ticketPrice;
 }
